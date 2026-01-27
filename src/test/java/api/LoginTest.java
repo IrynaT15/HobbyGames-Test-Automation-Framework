47187e6d3ffd261dcd21302da298a20315ade8b8
Iryna <irina.tainikova@gmail.com>
2026-01-28 00:09:19 +0100

    add testLoginWithEmptyPasswordAndProvidedEmail() test

--- src/test/java/api/LoginTest.java
+++ src/test/java/api/LoginTest.java
@@ -3,8 +3,7 @@ package api;
 import by.hobbygames.api.*;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
     @Test
@@ -40,6 +39,23 @@ public class LoginTest {
                         userAuthService.getMessage("errors.password")),
                 () -> assertEquals("Введите телефон или электронную почту",
                         userAuthService.getMessage("errors.login")),
+                () -> assertEquals("false",
+                        userAuthService.getMessage("success"))
+        );
+    }
+
+    @Test
+    public void testLoginWithEmptyPasswordAndProvidedEmail() {
+        UserAuthService userAuthService = new UserAuthService();
+        userAuthService.doRequest("it.19012026@gmail.com", "", "email");
+        userAuthService.printResponse();
+
+        assertAll("Login test",
+                () -> assertEquals(200, userAuthService.getStatusCode()),
+                () -> assertEquals("Введите пароль",
+                        userAuthService.getMessage("errors.password")),
+                () -> assertNull(userAuthService.getMessage("errors.phone")),
+                () -> assertNull(userAuthService.getMessage("errors.login")),
                 () -> assertEquals("false",
                         userAuthService.getMessage("success"))
         );
