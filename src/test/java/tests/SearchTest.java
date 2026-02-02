5d8d454bb6c4ee5524d70c3c0a90afa8c2c93cb1
Iryna <irina.tainikova@gmail.com>
2026-02-02 22:43:52 +0100

    add @Nested tests in SearchTest, code refactoring

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -6,54 +6,64 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchTest {
-    private HomePage homePage;
-    private SearchPage searchPage;
 
-    @BeforeEach
-    public void setupDriverAndOpenHomePageAndCloseCookieAlertAndClickLoginButton() {
-        homePage = new HomePage();
-        homePage.open();
-        homePage.clickCookieAlertClose();
-        searchPage = new SearchPage();
+    @Nested
+    public class SearchTestsWithoutSetup {
+        private SearchPage searchPage;
+
+        @BeforeEach
+        public void start() {
+            searchPage = new SearchPage();
+        }
+
+        @AfterAll
+        public static void quitDriver() {
+            Driver.quit();
+        }
+
+        @ParameterizedTest
+        @MethodSource("testdata.urls.PagesUrlProvider#providePagesUrl")
+        public void testSearchFieldAndSearchButtonArePresentOnDifferentPages(String page) {
+            searchPage.open(page);
+            assertAll("Search field and Search button on the page",
+                    () -> assertTrue(searchPage.isSearchFieldDisplayed(),
+                            "The Search field is not displayed on the page"),
+                    () -> assertTrue(searchPage.isSearchButtonDisplayed(),
+                            "The Search button is not displayed on the page")
+            );
+        }
+
     }
 
-    @AfterEach
-    public void quitDriver() {
-        Driver.quit();
-    }
+    @Nested
+    public class SearchTestsWithSetup {
+        private HomePage homePage;
+        private SearchPage searchPage;
 
-    @Test
-    public void testSearchFieldAndSearchButtonArePresentOnPage() {
-        assertAll("Search field and Search button on the page",
-                () -> Assertions.assertTrue(searchPage.isSearchFieldDisplayed(),
-                        "The Search field is not displayed on the page"),
-                () -> Assertions.assertTrue(searchPage.isSearchButtonDisplayed(),
-                        "The Search button is not displayed on the page")
-        );
-    }
+        @BeforeEach
+        public void setupDriverAndOpenHomePageAndCloseCookieAlertAndClickLoginButton() {
+            homePage = new HomePage();
+            homePage.open();
+            homePage.clickCookieAlertClose();
+            searchPage = new SearchPage();
+        }
 
-    @ParameterizedTest
-    @MethodSource("testdata.urls.PagesUrlProvider#providePagesUrl")
-    public void testSearchFieldAndSearchButtonArePresentOnDifferentPages(String page) {
-        searchPage.open(page);
-        assertAll("Search field and Search button on the page",
-                () -> Assertions.assertTrue(searchPage.isSearchFieldDisplayed(),
-                        "The Search field is not displayed on the page"),
-                () -> Assertions.assertTrue(searchPage.isSearchButtonDisplayed(),
-                        "The Search button is not displayed on the page")
-        );
-    }
+        @AfterEach
+        public void quitDriver() {
+            Driver.quit();
+        }
 
-    @Test
-    public void testSearchWithoutKeyword() {
-        searchPage.clickSearchButton();
-        assertAll("Search result for search without keyword",
-                () -> Assertions.assertEquals(searchPage.SEARCH_RESULTS_PAGE_URL, searchPage.getCurrentUrl()),
-                () -> Assertions.assertEquals(searchPage.SEARCH_RESULTS_PAGE_TITLE, searchPage.getPageTitleText()),
-                () -> Assertions.assertTrue(searchPage.getNumberOfFoundItemsText().contains("товаров"))
-        );
+        @Test
+        public void testSearchWithoutKeyword() {
+            searchPage.clickSearchButton();
+            assertAll("Search result for search without keyword",
+                    () -> assertEquals(searchPage.SEARCH_RESULTS_PAGE_URL, searchPage.getCurrentUrl()),
+                    () -> assertEquals(searchPage.SEARCH_RESULTS_PAGE_TITLE, searchPage.getPageTitleText()),
+                    () -> assertTrue(searchPage.getNumberOfFoundItemsText().contains("товаров"))
+            );
+        }
     }
 }
