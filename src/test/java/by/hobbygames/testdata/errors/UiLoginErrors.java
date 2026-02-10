6548c0851b6d01f58f3077f112b7b7e5f98b4160
Iryna <irina.tainikova@gmail.com>
2026-02-10 18:46:35 +0100

    code refactoring

--- src/test/java/by/hobbygames/testdata/errors/UiLoginErrors.java
+++ src/test/java/by/hobbygames/testdata/errors/UiLoginErrors.java
@@ -4,7 +4,6 @@ public class UiLoginErrors {
     public static final String LOGIN_IS_NOT_PROVIDED = "Введите телефон или электронную почту";
     public static final String PASSWORD_IS_NOT_PROVIDED = "Введите пароль";
     public static final String LOGIN_WITH_NOT_REGISTERED_EMAIL = "Неверный телефон/e-mail";
-    public static final String LOGIN_WITH_NOT_REGISTERED_PHONE = "";
     public static final String LOGIN_WITH_INVALID_EMAIL = "Введённые данные некорректны";
     public static final String PASSWORD_IS_WRONG = "Неверный пароль";
 }
--- src/test/java/by/hobbygames/testdata/urls/PagesUrlProvider.java
+++ src/test/java/by/hobbygames/testdata/urls/PagesUrlProvider.java
@@ -3,8 +3,6 @@ package by.hobbygames.testdata.urls;
 import java.util.stream.*;
 
 public class PagesUrlProvider {
-
-
     public static Stream<String> providePagesUrl() {
         return Stream.of(
                 "https://hobbygames.by/",
